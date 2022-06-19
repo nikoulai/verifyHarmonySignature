279a467f8274fa984a6368478a8da2b4f1d1f802
Nikos Iliakis <nikoulai@users.noreply.github.com>
2022-06-19 19:21:13 +0000

    Remove and commnent code

--- main.go
+++ main.go
@@ -14,11 +14,8 @@ import (
 
 
 func main() {
-	bls_core.Init(bls_core.BLS12_381)
 
-	// fmt.Println("Hello, world.")
-	// // 5 = BLS12_381
-	if bls_core.Init(5) != nil {
+	if bls_core.Init(bls_core.BLS12_381) != nil {
 		fmt.Println("fatal init")
 	}
 	// if bls.SetETHmode(0) != nil {
@@ -46,27 +43,22 @@ func main() {
 	};
 	var signature =
 	"c247e7747f51581691af06c6f3d80a1dd5d37c6663511079815ea82aeeb5cc5331e1517f378a9e5de04bdacc9f4ab804478ad77af332437b54bd2d4e08d3e50d539b020029c516338a3618501de570469c37bd9f4a4e4bcc61ab217fd08fd187"
+
 	var coreSig bls_core.Sign;
 	coreSig.DeserializeHexStr(signature)
 
 	var sigBytes bls.SerializedSignature;
 	copy(sigBytes[:], coreSig.Serialize())
-	// fmt.Println(len(sigBytes))
-	_ = signature
-	_ = signers
-	// _ = sigBytes
-	_ = coreSig;
+	
 
 	var aggregatePublic = &bls_core.PublicKey{}
 	for _, signer := range signers {
-		// fmt.Printf("signer= %v\n", signer)
 		var gen bls_core.PublicKey;
 		gen.DeserializeHexStr(signer)
 		aggregatePublic.Add(&gen)
 	    }
 	var blockHash = 
 	"0x01fc8590d17fbf7eed40deb9f991eefce89e8ddcfd3f9bb9bc7d4f480a53aee8"
-	// // _ = signers;
 	blockNumber := uint64(1);
 	blockNumBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(blockNumBytes, blockNumber)
@@ -79,76 +71,31 @@ func main() {
 
 	var res = coreSig.VerifyHash(aggregatePublic,commitPayload)
 	
-	// var multiSign = bls.Sign{};
-	// multiSign.SetHexString(signature)
-	/**************/
-	// var sigBytes bls.SerializedSignature;
-	// sigBytes.
-	// aggSig := bls_core.Sign{}
-	// err := aggSig.Deserialize(sigBytes[:])
-	/**************/
-
-	// var res = multiSign.VerifyHash(aggregatePublic,commitPayload)
-
 	fmt.Printf("res=%v\n", res)
 
 
-	var id bls_core.ID
-		err := id.SetLittleEndian([]byte{6, 5, 4, 3, 2, 1})
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("id :", id.SerializeToHexStr())
-		var id2 bls_core.ID
-		err = id2.DeserializeHexStr(id.SerializeToHexStr())
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Print("create secret key")
-		m := "this is a bls sample for go"
-		var sec bls_core.SecretKey
-		sec.SetByCSPRNG()
-		fmt.Println("sec:", sec.SerializeToHexStr())
-		fmt.Println("create public key")
-		pub := sec.GetPublicKey()
-		fmt.Println("pub:", pub.SerializeToHexStr())
-		sign := sec.Sign(m)
-		fmt.Println("sign:", sign.SerializeToHexStr())
-		if !sign.Verify(pub, m) {
-			fmt.Println("Signature does not verify")
-		}
-	// var gen bls.PublicKey
-	// gen.SetHexString("1 4f58f3d9ee829f9a853f80b0e32c2981be883a537f0c21ad4af17be22e6e9959915ec21b7f9d8cc4c7315f31f3600e5 1212110eb10dbc575bccc44dcd77400f38282c4728b5efac69c0b4c9011bd27b8ed608acd81f027039216a291ac636a8")
-	// bls.SetGeneratorOfPublicKey(&gen)
-
-	// var sec bls.SecretKey
-	// bytes := []byte{185, 10, 0, 237, 196, 231, 235, 253, 95, 221, 36, 224, 109, 252, 10, 222, 14, 197, 82, 107, 220, 29, 208, 123, 169, 190, 98, 181, 240, 198, 35, 98}
-	// sec.Deserialize(bytes)
-	// sig := sec.SignHash(bytes)
-	// fmt.Printf("sig=%v\n", sig.SerializeToHexStr())
-	// //	assertBool("check sig", byteToHexStr(sig.serialize()).equals("2f4ff940216b2f13d75a231b988cd16ef22b45a4709df3461d9baeebfeaafeb54fad86ea7465212f35ceb0af6fe86b1828cf6de9099cefe233d97e0523ba6c0f5eecf4db71f7b1ae08cd098547946abbd0329fdac14d27102f2a1891e9188a19"));A
-
-	// sec.DeserializeHexStr("d243f3f029c188a5b1c4c098f5719cbc967184ef962b5c5d6c72693c92c1f725")
-	// pub := sec.GetPublicKey()
-	// fmt.Printf("pub=%v\n", pub.SerializeToHexStr())
-	// if pub.SerializeToHexStr() != "15ad529698be1f6164fd50416d1991a04b977d9014b43ae9d014ca50ae634829182632d54c7188b3a53e0b77ae4c9e87" {
-	// 	fmt.Println("bad pub")
-	// }
-	// msg, _ := hex.DecodeString("1100000000000000000000000000000000000000000000000000000000000031")
-	// sig = sec.SignByte(msg)
-	// if sig.SerializeToHexStr() != "2f2071370054ded61081a069dc657d7111ae909fc08a38ae1f3a967c4e4c1b97b7d2ee8adc512ded7213dc0ab2c9db158451eca54a4db26ec013e108d561d8e0eede883b73074d49143ea8e9291903aabce2a2b4f153f99822e4512dac86ae18" {
-	// 	fmt.Println("bad sig")
-	// }
-	// sig = sec.SignHash(msg)
-	// if sig.SerializeToHexStr() != "807f6fb074cfdd0318501ef894b127a3f71754745dcff2163dd128091efd065c4ac419e3d7d5428a0b94ada19de657176fb4ccfdb3105f869d6351e503ba20e9d6ef55f179510941db4d131c3678e9d9316090c99bd7d62107d83f6fe5f6f115" {
-	// 	fmt.Println("bad sig2")
-
-	// }
+	// var id bls_core.ID
+	// 	err := id.SetLittleEndian([]byte{6, 5, 4, 3, 2, 1})
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println("id :", id.SerializeToHexStr())
+	// 	var id2 bls_core.ID
+	// 	err = id2.DeserializeHexStr(id.SerializeToHexStr())
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Print("create secret key")
+	// 	m := "this is a bls sample for go"
+	// 	var sec bls_core.SecretKey
+	// 	sec.SetByCSPRNG()
+	// 	fmt.Println("sec:", sec.SerializeToHexStr())
+	// 	fmt.Println("create public key")
+	// 	pub := sec.GetPublicKey()
+	// 	fmt.Println("pub:", pub.SerializeToHexStr())
+	// 	sign := sec.Sign(m)
+	// 	fmt.Println("sign:", sign.SerializeToHexStr())
+	// 	if !sign.Verify(pub, m) {
+	// 		fmt.Println("Signature does not verify")
+	// 	}
 }
-func Reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-	    runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-    }
